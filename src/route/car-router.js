24bf66b2e40a8c8093144339a8d376a633fa8c0a
Devin Cunningham <dtyler.cunningham@gmail.com>
2018-06-21 08:34:08 -0700

    server runs

--- src/route/car-router.js
+++ src/route/car-router.js
@@ -1,38 +1,38 @@
 'use strict';
 
-const Note = require('../model/car');
+const Car = require('../model/car');
 const logger = require('../lib/logger');
 const responses = require('../lib/response');
 
 module.exports = (router) => {
-    router.post('api/v1/car', (req, res) => {
-        logger.log(logger.INFO, 'Route Note: POST /api/v1/car');
-        const newCar = new Car(req.body);
-        newCar.save()
-        .then((car) => {
-            responses.sendJSON((response, 200, car);
-            return undefined
-        })
-        .catch((err) => {
-            logger.log(logger.INFO, `Route Note: bad req ${JSON.stringify(err.message)}`);
-            responses.sendError(response, 400, err.message);
-            return undefined;
-        });
-        });
-    
-    router.get('/api/v1/car', (req, res) => {
-        if (!req.url.query.id) {
-            responses.sendError(response, 404, 'Your request needs an id')
-            return undefined;
-        }
-        Car.findOne(req.url.query.id)
-        .then((note) => {
-            responses.sendJSON(response, 200, car)
-        })
-        .catch((err) => {
-            console.log(err);
-            responses.sendError(response, 404, err.message);
-        })
+  router.post('api/v1/car', (req, res) => {
+    logger.log(logger.INFO, 'Route Car: POST /api/v1/car');
+    const newCar = new Car(req.body);
+    newCar.save()
+      .then((car) => {
+        responses.sendJSON(res, 200, car);
         return undefined;
-    })
+      })
+      .catch((err) => {
+        logger.log(logger.INFO, `Route Car: bad req ${JSON.stringify(err.message)}`);
+        responses.sendError(res, 400, err.message);
+        return undefined;
+      });
+  });
+    
+  router.get('/api/v1/car', (req, res) => {
+    if (!req.url.query.id) {
+      responses.sendError(res, 404, 'Your request needs an id');
+      return undefined;
+    }
+    Car.findOne(req.url.query.id)
+      .then((car) => {
+        responses.sendJSON(res, 200, car);
+      })
+      .catch((err) => {
+        console.log(err);
+        responses.sendError(res, 404, err.message);
+      });
+    return undefined;
+  });
 };
